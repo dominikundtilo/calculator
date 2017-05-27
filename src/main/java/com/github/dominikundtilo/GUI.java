7b8b1f2304f0f9ada81147306848366c48766f82
Tilo Spannagel <development@tilosp.de>
2017-05-27 14:00:24 +0200

    Fixed build

--- src/main/java/com/github/dominikundtilo/GUI.java
+++ src/main/java/com/github/dominikundtilo/GUI.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 /**
  * Created by Dominik on 23.05.2017.
  */
+
 public class GUI extends JFrame {
     private JPanel productionPanel;
     private JPanel configPanel;
@@ -19,32 +20,11 @@ public class GUI extends JFrame {
     final String[] furnaces = {"Stonefurnace", "Steelfurnace", "Electronicfurnace"};
     final String[] assemblers = {"Assembling Maschien 1", "Assembling Maschien 2", "Assembling Maschien 3"};
 
-
-
-    private void createUIComponents() {
-        furnaceComboBox = new JComboBox();
-        assemblingComboBox = new JComboBox();
-
-        assemblingComboBox.setVisible(true);
-
-        assemblingComboBox.addItem(Main.getMachineName(0));
-
-
-
-        for(int x = 0; x < furnaces.length; x++)furnaceComboBox.addItem(furnaces[x]);
-        for(int x = 0; x < assemblers.length; x++)assemblingComboBox.addItem(assemblers[x]);
-
-
-
-    }
-
-    public GUI (){
-        super();
+    public GUI() {
+        super("Example name");
         setContentPane(panel1);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-
-
+        pack();
     }
-
 }
