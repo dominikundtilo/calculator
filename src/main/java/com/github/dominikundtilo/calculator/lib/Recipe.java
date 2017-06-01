3aada18195d8e285c62cc40d321e1a2fa5962516
Tilo Spannagel <development@tilosp.de>
2017-06-01 22:03:42 +0200

    Added machine tiers

--- src/main/java/com/github/dominikundtilo/calculator/lib/Recipe.java
+++ src/main/java/com/github/dominikundtilo/calculator/lib/Recipe.java
@@ -28,8 +28,27 @@ public class Recipe {
         return energy;
     }
 
-    public double computeAmount(double amount) {
-        return amount * energy / products[0].getAmount() * products[0].getProbability();
+    public double computeAmount(double amount, int assemblingTier, int smeltingTier) {
+        return amount * energy / products[0].getAmount() * products[0].getProbability() / getCraftingSpeed(assemblingTier, smeltingTier);
+    }
+
+    private double getCraftingSpeed(int assemblingTier, int smeltingTier) {
+        if (category.equals("chemistry"))
+            return 1.25;
+        else if (category.equals("centrifuging"))
+            return 0.75;
+        else if (category.equals("crafting") || category.equals("advanced-crafting") || category.equals("crafting-with-fluid"))
+            switch (assemblingTier) {
+                case 1: return 0.5;
+                case 2: return 0.75;
+                case 3: return 1.25;
+            }
+        else if (category.equals("smelting"))
+            switch (smeltingTier) {
+                case 1: return 1;
+                case 2: case 3: return 2;
+            }
+        return 0;
     }
 
     @Override
